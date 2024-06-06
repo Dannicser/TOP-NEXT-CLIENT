4e6a2162dd806de2952175d8ce42e784f1b91970
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-06-06 12:54:07 +0300

    feat: add css vars

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import { AppProps } from "next/dist/shared/lib/router/router";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>My Top</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
