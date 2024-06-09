a7bf41d2ed53083ce37dbc66ce2f44c0ca3ee5db
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-06-09 13:28:20 +0300

    feat: add Footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { IFooterProps } from "./Footer.props";
 
-export const Footer: React.FC<IFooterProps> = ({ ...props }): JSX.Element => {
-  return <div {...props}>Footer</div>;
+import cn from "classnames";
+
+import styles from "./Footer.module.css";
+import { format } from "date-fns";
+
+export const Footer: React.FC<IFooterProps> = ({ className, ...props }): JSX.Element => {
+  return (
+    <footer className={cn(styles.footer, className)} {...props}>
+      <div>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</div>
+      <a>Пользовательское соглашение</a>
+      <a>Политика конфиденциальности</a>
+    </footer>
+  );
 };
